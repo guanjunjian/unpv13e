26cd07bbe507c98e0b88333b33951bb987dd4b6c
GJUNJIAN <215173975@qq.com>
2018-04-26 11:55:25 +0800

    update

--- select/strcliselect01.c
+++ select/strcliselect01.c
@@ -6,23 +6,34 @@ str_cli(FILE *fp, int sockfd)
 	int			maxfdp1;
 	fd_set		rset;
 	char		sendline[MAXLINE], recvline[MAXLINE];
-
+	//初始化描述符集
 	FD_ZERO(&rset);
 	for ( ; ; ) {
+		//fileno(fp)把标准I/O文件指针fp转换为对应的描述符
 		FD_SET(fileno(fp), &rset);
+		//将套接字描述符设为关心
 		FD_SET(sockfd, &rset);
+		//获取两个描述符中的较大值，并+1计算出maxfdp1值
 		maxfdp1 = max(fileno(fp), sockfd) + 1;
+		//包裹函数Select，增加了返回值小于0的错误处理
+		//将readset和writeset设为NULL，表示不感兴趣
+		//将timeout设为NULL，表示永远等待，直到有描述符准备好
 		Select(maxfdp1, &rset, NULL, NULL, NULL);
-
+		//如果套接字可读
 		if (FD_ISSET(sockfd, &rset)) {	/* socket is readable */
+			//读取套接字
 			if (Readline(sockfd, recvline, MAXLINE) == 0)
+				//如果套接字返回0，表示对端进程终止，输出错误并退出客户进程
 				err_quit("str_cli: server terminated prematurely");
+			//输出套接字读入的结果
 			Fputs(recvline, stdout);
 		}
-
+		//如果标准输入可读
 		if (FD_ISSET(fileno(fp), &rset)) {  /* input is readable */
+			//读取标准输入
 			if (Fgets(sendline, MAXLINE, fp) == NULL)
 				return;		/* all done */
+			//将标准输入读到的内容写入套接字
 			Writen(sockfd, sendline, strlen(sendline));
 		}
 	}
